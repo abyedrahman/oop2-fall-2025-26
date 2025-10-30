ca748ba81cda3c84417db2b45fee6ec6cb5005ef
abyedrahman <abidrahman78902@gmail.com>
2025-10-30 21:14:33 +0600

    23-51846-2

---
+++ Task04/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a = 10;
+        int b = 20;
+
+        Console.WriteLine($"Before swapping: a = {a}, b = {b}");
+
+        // Swapping using arithmetic (no temp variable)
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        Console.WriteLine($"After swapping: a = {a}, b = {b}");
+    }
+}
